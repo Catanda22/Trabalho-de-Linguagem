af6000078bbeb2cd74a771ed215d81b743828e6f
Catanda22 <catandafernando3@gmail.com>
2026-04-11 22:01:50 +0100

    Add TCP server functionality for chat application
    Implemented a TCP server that accepts client connections and broadcasts messages.

--- ChatSockets/src/ServidorChat.java
+++ ChatSockets/src/ServidorChat.java
@@ -1,1 +1,45 @@
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ServidorChat {
+
+    public static Set<ClientHandler> clientes = new HashSet<>();
+
+    public static void main(String[] args) {
+        int porta = 11000;
+
+//criacao do servidor TCP
+        try {
+            ServerSocket servidor = new ServerSocket(porta);
+
+            // a funcao contiua executando e clientes poddem conectar-se
+
+            while (true) {
+                Socket socket = servidor.accept();
+                //criacao do handler
+
+                ClientHandler handler = new ClientHandler(socket);
+                clientes.add(handler);
+
+                //iniciar thread
+
+                new Thread(handler).start();
+            }
+            // tratar erros
+
+        } catch (IOException e) {
+            System.out.println("failure" + e.getMessage());
+
+        }
+    }
+// transmitir mensagem
+    public static void broadcast(String sms, ClientHandler processar) {
+        for (ClientHandler cliente : clientes) {
+            if (cliente != processar) {
+                cliente.enviar(sms);
+            }
+        }
+    }
+}
